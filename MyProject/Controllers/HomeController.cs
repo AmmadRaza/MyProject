6baa8feffbf59cc13a45bc64c30ca48687d5e6ba
AmmadRaza <ammadraza001@gmail.com>
2017-08-01 12:58:18 -0700

    Commit Message

---
+++ MyProject/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+
+        public bool HireDoctor()
+        {
+            return true;
+        }
+
+        [HttpGet]
+        public bool PatientRegister()
+        {
+            return true;
+        }
+
+        [HttpGet]
+
+        public bool PatientAppoinment()
+        {
+            return true;
+        }
+
+        [HttpGet]
+
+        public bool PanelPatient()
+        {
+            return true;
+        }
+
+        [HttpGet]
+
+        public int Billing()
+        {
+            return 0;
+
+        }
+    }
+}
